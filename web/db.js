a8e554909881720c054601da8cd7ea05592c9473
Joe Modjeski <joemodjeski@Joes-MacBook-Pro.local>
2013-10-15 19:56:03 -0700

    drying out api handler

--- web/db.js
+++ web/db.js
@@ -2,14 +2,27 @@ var url = require('./config').mongodb.url,
   mongoose = require('mongoose'),
   Schema = require('mongoose').Schema;
 
+var defaultJsonTransform = function (doc, ret, options) {
+  // standardize on regular 'id' field
+  ret.id = ret._id;
+  // TODO: remove _id once all obects are migrated
+  //delete ret._id;
+};
+
+var defaultSchemaOptions = {
+  toJSON: {
+    transform: defaultJsonTransform
+  }
+};
+
 var meetingSchema = new Schema({
   title: 'string',
   participants: [new Schema({
     type: String,
     rate: Number,
     count: Number 
-  })]
-});
+  }, defaultSchemaOptions)]
+}, defaultSchemaOptions);
 
 var meetingLogSchema = new Schema({
   startTime: Date,
@@ -22,8 +35,8 @@ var meetingLogSchema = new Schema({
     rate: Number,
     count: Number,
     totalCost: Number
-  })]
-});
+  }, defaultSchemaOptions)]
+}, defaultSchemaOptions);
 
 mongoose.connect(url);
 var Meetings = mongoose.model('Meetings', meetingSchema);
--- web/routes/api.js
+++ web/routes/api.js
@@ -3,133 +3,146 @@ var _ = require('underscore'),
   db = require('../db'),
   url = require('url');
 
-  var findModel = function (repo, id, segments, callback, query) {
-    if (!id)
-      repo.find(query, callback);
-    else
-      repo.findById(id, function (err, model) {
-        if (segments.length > 0)
-          findSubModel(model, segments, callback);
-        else
-          callback(err, model);
-      });
-  }
-
-  var findSubModel = function (model, segments, callback) {
-    if (segments.length === 1) {
-      callback(undefined, model[segments[0]]);
-    } else if (model[segments[0]].findById) {
-      model[segments[0]].findById(segments[1], function (err, m) {
-        if (!err) {
-          model = m;
-          if (segments.length > 2) {
-            segments = segments.slice(2, segments.length);
-            findSubModel(model, segments, callback);
-          } else {
-            callback(err, model);
-          }
-        }
-      });
-    } else {
-      model = model[segments[0]].id(segments[1]);
-      if (segments.length > 2) {
-        segments = segments.slice(2, segments.length);
-        findSubModel(model, segments, callback);
-      } else {
-        callback(undefined, model);
-      }
-    }
-  }
-
-  var get = function(repo, req, res, next){
-    var urlParts = url.parse(req.url).pathname.split(/\//);
-    var id = urlParts[2];
-    var query = req.query || {};
-    urlParts = urlParts.slice(3, urlParts.length);
-    findModel(repo, id, urlParts, function (err, model) {
-      res.send(err ? 500 : 200, err || model);
-    }, query);
+  var appendUrl = function(url, suffix){
+    if(url[url.length -1] == '/') return url + suffix;
+    else return url +'/'+suffix;
   };
 
+  var parseUrl = function(req){
+    // pathname starts with '/' so we strip off the empty item
+    // at the begining of the segments array
+    var parts = url.parse(req.url).pathname.split(/\//).slice(1);
+    var paths = [];
+    for(var i = 0; i < parts.length; i++)
+    {
+      var model = {
+        segment: parts[i],
+        id: (i++ < parts.length ? parts[i] : undefined)
+      };
+      paths.push(model);
+    }
+
+    return paths;
+  };
+
+  // keep shifting paths to find the deepest model
+  // requested, invoke fn on it when found
+  var findchild = function(err, paths, model, fn){
+    var path = paths.shift();
+    var child = model[path.segment];
+    if(path.id) child = child.id(path.id);
+    if(paths.length != 0) findchild(err, paths, child, fn);
+    else fn(err, child);
+  };
+
+  var finalHandler = function(res, blockResult){
+    return function(err, result){
+      if(err) res.send(500, err);
+      else res.send(200, blockResult ? undefined : result);
+    }
+  };
+
+  var get = function(repo, req, res, next){
+    var handler = finalHandler(res);
+    var paths = parseUrl(req);
+    var path = paths.shift();
+
+    if(path.id) {
+      repo.findById(path.id, function(err, model){
+        if(paths.length == 0) handler(err, model);
+        else findchild(err, paths, model, handler);
+      });
+    } else {
+      repo.find(req.query, handler);
+    }
+  };
+
+
   var put = function(repo, req, res, next){
-    var urlParts = req.url.split(/\//);
-    var id = urlParts[2];
-    findModel(repo, id, [], function (err, model) {
-      var rootModel = model;
-      if (urlParts.length < 3) {
-        rootModel = _.extend(rootModel, req.body);
-        rootModel.save(function (err, updatedRoot, recordCount) {
-          res.send(err ? 500 : 200);
-        });
-      } else {
-        findSubModel(rootModel, urlParts.slice(3, urlParts.length), function (err, model) {
-          model = _.extend(model, req.body);
-          rootModel.save(function (err, updatedRoot, recordCount) {
-            res.send(err ? 500 : 200);
+    var handler = finalHandler(res);
+    var paths = parseUrl(req)
+    var path = paths.shift();
+
+    var save = function(parent) {
+      return function(err, model){
+        if(err) handler(err)
+        else {
+          _.extend(model, req.body);
+          parent.save(function(err){
+            handler(err, model);
           });
-        });
-      }
+        }
+      };
+    };
+
+    repo.findById(path.id, function(err, model){
+      var fn = save(model);
+      if(paths.length == 0) fn(err, model);
+      else findchild(err, paths, model, fn);
     });
   };
 
+
   var post = function(repo, req, res){
-    var urlParts = url.parse(req.url).pathname.split(/\//);
-    if (urlParts.length < 3) {
-      repo.create(req.body, {}, function(err, result){
-        res.set({
-          'Location': result._id
-        });
-        res.send(err ? 500 : 201, err || result);
-      });
-    } else {
-      var id = urlParts[2];
-      var rootModel = {};
-      findModel(repo, id, [], function (err, model) {
-        rootModel = model;
-        if (err) res.send(500, err);
+    var errorHandler = finalHandler(res);
+    var paths = parseUrl(req);
+    var path = paths.shift();
+
+    var created = function(err, model){
+        if(err) res.send(500, err);
         else {
-          findSubModel(rootModel, urlParts.slice(3, urlParts.length), function (err, model) {
-            if (Object.prototype.toString.call(model) === '[object Array]') {
-              model.push(req.body);
-              rootModel.save(function (err, updatedRoot, recordCount) {
-                res.set({
-                  'Location': model[model.length - 1]._id
-                });
-                res.send(err ? 500 : 201, err);
-              });
-            }
-          }); 
+          res.set({
+            'Location': appendUrl('/api' + req.url, model.id)
+          });
+          res.send(201, model);
         }
+    };
+
+    if(path.id)
+    {
+      repo.findById(path.id, function(err, parent){
+        if(paths.length == 0) errorHandler(new Error("Invalid path to child model"));
+        else findchild(err, paths, parent, function(err, set){
+          if(err) errorHandler(err);
+          else {
+            var model = set.create(req.body);
+            set.push(model);
+            parent.save(function(err, m){
+              // override created to send in child model
+              created(err, model);
+            });
+          }
+        });
       });
     }
-  }  
+    else
+    {
+      var model = new repo(req.body);
+      model.save(created); 
+    }
+  };
+
 
   var del = function(repo, req, res, next){
-    var urlParts = req.url.split(/\//);
-    var id = urlParts[2];
-    var rootModel = {};
-    findModel(repo, id, [], function (err, model) {
-      rootModel = model;
-      if (err) res.send(500, err);
-      else {
-        if (urlParts.length < 3) {
-          rootModel.remove({ _id: rootModel._id }, function (err) {
-            if (err) res.send(500, err);
-            else res.send(200);
-          });
-        } else {
-          findSubModel(rootModel, urlParts.slice(3, urlParts.length), function (err, model) {
-            if (err) res.send(500, err);
-            else {
-              model.remove({ _id: model._id }, function (err) {
-                if (err) res.send(500, err);
-                else res.send(200);
-              });
-            }
-          });
+    var handler = finalHandler(res, true);
+    var paths = parseUrl(req);
+    var path = paths.shift();
+
+    var remove = function(parent) {
+      return function(err, model){
+        if(err || !model) handler(err); // errored out(500), or no matching model(still 200)
+        else {
+          model.remove();
+          parent.save(handler);
         }
-      }
-    })
+      };
+    }
+
+    repo.findById(path.id, function(err, model){
+      var fn = remove(model);
+      if(paths.length == 0) fn(err, model);
+      else findchild(err, paths, model, fn);
+    });
   };
 
   exports = module.exports = function(req, res, next){
