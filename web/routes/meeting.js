d586531034613db775574b0774342f50e45ff885
Joe Modjeski <joemodjeski@Joes-MacBook-Pro.local>
2013-10-16 10:31:39 -0700

    updating meeting route to load model from db

--- web/routes/meeting.js
+++ web/routes/meeting.js
@@ -1,6 +1,16 @@
+var db = require('../db'),
+  _ = require('lodash');
+
+var handler = function(res, fn){
+  return function(err) {
+    if(err) res.send(500, err);
+    else fn.apply(this, arguments);
+  };
+};
 
 exports = module.exports = function(req, res, next){
-
-	res.model.id = req.params.id;
-	res.render('meeting', res.model);
+  db.meetings.findById(req.params.id, handler(res, function(err, model){
+    res.model.meeting = model;
+    res.render('meeting', res.model);
+  }));
 };
